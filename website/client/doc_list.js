50ac732bcd839e8c4fe0b8a7db62d04089f262cf
ArhanCrane <123109947+ArhanCrane@users.noreply.github.com>
2025-09-17 21:32:51 +0300

    Update doc_list.js

--- website/client/doc_list.js
+++ website/client/doc_list.js
@@ -1,10 +1,27 @@
-console.log(localStorage.getItem('DocsList'));
+//console.log(localStorage.getItem('DocsList'));
+const socket_chat = new WebSocket('ws://localhost:43249/docslist');
 
+socket_chat.addEventListener('open', (event) => {
+    console.log('WebSocket connection opened:', event);
+    socket_chat.send(localStorage.getItem('login'));
+    //console.log(localStorage.getItem('login'));
+})
+socket_chat.addEventListener('message', (event) => {
+  console.log('Message from server:', event.data);
+  localStorage.setItem('DocsList', event.data);
+  drawList();
+});
+socket_chat.addEventListener('close', (event) => {
+  console.log('WebSocket connection closed:', event);
+});
+socket_chat.addEventListener('error', (event) => {
+  console.error('WebSocket error:', event);
+});
 
 
 function drawList() {
     const list = document.getElementById('DocsList');
-
+    console.log("drawList()");
     let DocsNames = [];    
     var line = "";
     for (let i = 1; i < localStorage.getItem('DocsList').length; i++) {
@@ -21,12 +38,11 @@ function drawList() {
     //console.log(DocsNames);
     if (DocsNames[0] != "ull") {
 
-        for (let i = 0; i < DocsNames.length; i++) {
-            
+        for (let i = 0; i < DocsNames.length; i++) {            
             const line = document.createElement('button');
     
             line.innerText = DocsNames[i];
-            line.className = "my-button";        
+            line.className = "my-button";              
     
             line.style.position = 'absolute';
     
@@ -39,28 +55,25 @@ function drawList() {
             line.style.backgroundColor = 'grey';
     
             list.appendChild(line);
+            console.log(line.className);
         }    
     }
+
+    const buttons = document.getElementsByClassName("my-button");
+
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', (event) => {
+            console.log("click!");
+            EditDocument(event.target.innerText);
+            console.log(event.target.innerText);
+        });
+    };
 }
 
-drawList();
-
-const buttons = document.getElementsByClassName("my-button");
-//console.log(buttons.length);
-
-for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener('click', (event) => {
-        EditDocument(event.target.innerText);
-        console.log(event.target.innerText);
-    });
-};
-
-function EditDocument(title) {
-    
+function EditDocument(title) {    
     localStorage.setItem('DocsName', null);
     localStorage.setItem('DocsName', title);
-    window.location.href = "C:/Users/oooks/Desktop/JavaProject/client/edit.html";
-    
+    window.location.href = "C:/Users/oooks/Desktop/website/client/edit.html";
 }
 
 
